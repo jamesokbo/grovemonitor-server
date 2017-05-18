8f2cf3170ef9db48619edcde95ddaa311e9d2b2e
Jaime Okhuysen <jamesokbo@gmail.com>
2017-05-18 21:00:02 +0000

    deleteSensor functionality

---
+++ server/userSocketEvents/monitorEvents/monitorSettingsEvents/deleteSensor.js
@@ -0,0 +1,47 @@
+var timeoutCallback=require('timeout-callback');
+var mainRPiArrays=require('../../../mainRPiArrays.js');
+var Monitor=require('../../../models/monitor.js');
+var errors=require('../../../errors.js');
+var constants=require('../../../constants.js');
+
+module.exports=function(socket){
+    socket.on('deleteSensor',function(data,fn){
+        console.log("adding sensor");
+         Monitor.find({monitorID:data.monitorID},function(err,docs){
+            if(err){
+                throw err;
+            }
+            if(docs.length==1){
+                var mainRPiIndex=mainRPiArrays.mainRPiIDs.indexOf(docs[0].mainRPiID);
+                if(mainRPiIndex!=-1){
+                    mainRPiArrays.mainRPis[mainRPiIndex].emit('addSensor',data,
+                    timeoutCallback(constants.MAINRPI_TIMEOUT,function(err,res){
+                        if(err){
+                            fn(err);
+                        }
+                        else{
+                            if(res.status){
+                                var sensors=docs[0].sensors;
+                                sensors.push(data.newSensor);
+                                sensors.sort();
+                                Monitor.update({_id:data.monitorID},
+                                {$set:{'sensors':sensors}},function(err,doc){
+                                    if(err){
+                                        throw err;
+                                    }
+                                    fn(null,{status:true});
+                                });  
+                            }    
+                        }
+                    }));
+                }
+                else{
+                    fn(errors.s008.toString());        
+                }
+            }
+            else{
+                fn(errors.s007.toString());
+            }
+        });
+    });
+};
